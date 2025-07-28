e26cab359c6a4d741829353c3a48d43ac0bd2bf4
Kirill <k.shelestov@g.nsu.ru>
2025-07-28 11:06:02 +0700

    Change index.js

--- food-finder-backend/index.js
+++ food-finder-backend/index.js
@@ -6,19 +6,16 @@ import { recognizeImageText, parseOcrMenuText } from "./parsers/ocr.js";
 async function main() {
     // const dishes = await parseKhanBuzMenu();
     // console.log(dishes);
-
     // const categories = await parseSunnyDayCategories();
     // console.log(categories);
-
-    const vkImg = await getLatestPostImage();
-    console.log("VK first post image:", vkImg);
-
-    if (vkImg) {
-        const text = await recognizeImageText(vkImg);
-        console.log("Распознанный текст с картинки:", text);
-        const dishes = parseOcrMenuText(text);
-        console.log("Блюда:", dishes);
-    }
+    // const vkImg = await getLatestPostImage();
+    // console.log("VK first post image:", vkImg);
+    // if (vkImg) {
+    //     const text = await recognizeImageText(vkImg);
+    //     console.log("Распознанный текст с картинки:", text);
+    //     const dishes = parseOcrMenuText(text);
+    //     console.log("Блюда:", dishes);
+    // }
 }
 
 main();
